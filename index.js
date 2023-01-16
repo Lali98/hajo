ad1aa256352790558761d016e6e7e381a508c548
Lali98 <kisslajosbertalan77@gmail.com>
2023-01-16 11:02:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.get('/json', (req, res) => {
+    fs.readFile('./data/index.json', (err, data) => {
+        res.send(JSON.parse(data));
+    })
+})
+
+app.get('/json/:id', (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile('./data/index.json', (err, data) => {
+        const hajok = JSON.parse(data);
+        const hajobyId = hajok.find(index => index.id === Number(id));
+        if(!hajobyId) {
+            res.status(404).send({error: `id: ${id} not found`});
+            return;
+        }
+        res.send(hajobyId);
+    })
+})
+
+app.listen(3000);
